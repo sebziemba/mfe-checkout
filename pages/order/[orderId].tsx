09af93ded4d1dccfae9a54fafc5fe7a787036fe1
sebziemba <94920639+sebziemba@users.noreply.github.com>
2026-01-05 20:49:53 +0100

    Console LOG OrderId

--- pages/order/[orderId].tsx
+++ pages/order/[orderId].tsx
@@ -3,9 +3,15 @@ import { useEffect } from "react"
 import Order from "../Order"
 
 const OrderById: NextPage = () => {
+  // This one often survives even when console.log is stripped:
+  if (typeof window !== "undefined") {
+    console.error("[order page] render", window.location.href)
+  }
+
   useEffect(() => {
-    console.log("[order page] loaded", window.location.href)
+    console.error("[order page] useEffect", window.location.href)
   }, [])
+
   return <Order />
 }
 
